5502914f248a66d84dcc1caa1fc676b5019e8744
Cengiz Han <sonsair11@gmail.com>
2022-08-11 10:38:49 +0300

    Operation claim, User and Dto's was added

---
+++ Core/Concrete/OperationClaim.cs
@@ -0,0 +1,13 @@
+﻿using Core.Abstrac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Concrete
+{
+    public class OperationClaim :IEntity
+    {
+        public int OperationClaimId { get; set; }
+        public string OperationClaimName { get; set; }
+    }
+}
---
+++ Core/Concrete/UserOperationClaim.cs
@@ -0,0 +1,14 @@
+﻿using Core.Abstrac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Concrete
+{
+    public class UserOperationClaim : IEntity
+    {
+        public int UserOperationClaimId { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+    }
+}
