35f0a2ac6e32d4f3c0c7e7b12046356a3bbe03c6
Cengiz Han <sonsair11@gmail.com>
2022-08-11 16:52:31 +0300

    EntityFramework entities are added.

--- DataAccess/Concrete/EntityFramework/EfUserDal.cs
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Core.Concrete;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstrac;
+using DataAccess.Concrete.Contexts;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    internal class EfUserDal
+    public class EfUserDal : EfEntityRepositoryBase<User, NorthwindContext>, IUserDal
     {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context = new NorthwindContext())
+            {
+                var result = from operationClaim in context.OperationClaims
+                             join userOperationClaim in context.UserOperationClaims
+                             on operationClaim.OperationClaimId equals userOperationClaim.OperationClaimId
+                             where userOperationClaim.UserId == user.Id
+                             select new OperationClaim
+                             { OperationClaimId = operationClaim.OperationClaimId, OperationClaimName = operationClaim.OperationClaimName };
+                return result.ToList();
+
+            }
+        }
     }
 }
