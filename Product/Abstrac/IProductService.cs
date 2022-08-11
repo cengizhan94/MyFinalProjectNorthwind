6e8c8205b2b090e1189e2093f2f0ca69a13e058d
Cengiz Han <sonsair11@gmail.com>
2022-08-11 17:02:40 +0300

    Services added.

---
+++ Product/Abstrac/IProductService.cs
@@ -0,0 +1,22 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstrac
+{
+    public interface IProductService
+    {
+        IDataResult<List<Product>> GetAll();
+        IDataResult<Product> GetById(int productId);
+        IDataResult<List<Product>> GetAllByCategoryId(int id);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IResult Add(Product product);
+        IResult Update(Product product);
+        IResult Delete(Product product);
+        IResult AddTransactionalTest(Product product);
+    }
+}
---
+++ Product/Abstrac/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Core.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstrac
+{
+    public interface IUserService
+    {
+        List<OperationClaim>GetClaims(User user);
+        void add(User user);
+        User GetByMail(string mail);
+    }
+}
