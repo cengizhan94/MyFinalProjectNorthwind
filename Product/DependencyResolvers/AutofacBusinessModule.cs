d30dcb6a7471a659f9fea6a29b4f325f5f06d7d8
Cengiz Han <sonsair11@gmail.com>
2022-08-12 10:37:24 +0300

    DependencyResolvers added

---
+++ Product/DependencyResolvers/AutofacBusinessModule.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstrac;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using Core.Utilities.Security.JWT;
+using DataAccess.Abstrac;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Http;
+
+namespace Business.DependencyResolvers
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableClassInterceptors(new ProxyGenerationOptions()
+                
+            {
+                Selector = new AspectInterceptorSelector()
+            }).SingleInstance();
+        }
+    }
+}
